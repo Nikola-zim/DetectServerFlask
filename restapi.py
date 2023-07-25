00092269c05c68db0bc83b37e1af65ac45e301c5
Nikolazim <nikolazim.2020@gmail.com>
2023-07-25 21:00:02 +0300

    init praktikum

--- restapi.py
+++ restapi.py
@@ -1,6 +1,3 @@
-"""
-Run a rest API exposing the yolov5s object detection model
-"""
 import argparse
 import datetime
 import io
@@ -10,7 +7,8 @@ import json
 from PIL import Image
 
 import torch
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify
+from base64 import encodebytes
 
 app = Flask(__name__)
 
@@ -44,12 +42,21 @@ def predict():
         # Открытие файла изображения в двоичном режиме
         with open(img_savename, 'rb') as f:
             image_data = f.read()
-
         # Добавление файла изображения к объекту Response
         response.set_data(image_data)
+        encoded_img = get_response_image(img_savename)
+        AllData = {
+            "Image": encoded_img,
+            "Description": json.dumps(results.pandas().xyxy[0].to_json(orient="records"))
+        }
+        return jsonify(AllData)
 
-        return response
-
+def get_response_image(image_path):
+    pil_img = Image.open(image_path, mode='r') # reads the PIL image
+    byte_arr = io.BytesIO()
+    pil_img.save(byte_arr, format='PNG') # convert the PIL image to byte array
+    encoded_img = encodebytes(byte_arr.getvalue()).decode('ascii') # encode as base64
+    return encoded_img
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask api exposing yolov5 model")
@@ -59,4 +66,5 @@ if __name__ == "__main__":
 
     model_path = 'best.pt'
     model = torch.hub.load('D:/Programming/Python_prog/Diploma/YOLOV5_2502/yolov5-master', 'custom', path=model_path, force_reload=True, source='local')
+    model.conf = 0.50
     app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
